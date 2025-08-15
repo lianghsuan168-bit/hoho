d890337607dbdb62228e7ca657114e2ca05330ef
lianghsuan168-bit <lianghsuan168@gmail.com>
2025-08-15 16:28:31 +0800

    Update hoho.js

--- hoho.js
+++ hoho.js
@@ -1,4 +1,4 @@
-const CSV_PATH = './file.csv';
+const CSV_PATH = './customers.csv';
 
 let rows = [];               // 原始資料：[{customer, vendor}, ...]
 let customerMap = new Map(); // 精準查詢：customer(lower) -> vendor
@@ -99,3 +99,4 @@ document.getElementById('q').addEventListener('keydown', (e) => { if (e.key === 
 setupFilter();
 
 loadCSV();
+
